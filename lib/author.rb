c13bdac6c3fe83ba6a34449f0d1f0e9f3f7140bd
Derek phung <dragevards@gmail.com>
2020-04-29 20:15:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,10 +1,20 @@
 class Author 
   
-  attr_accessor :name
+  attr_accessor :name, :posts
   
   def initialize(name)
     @name = name
   end 
   
+  
+  
   def add_post(post)
-    post.name == self 
+    post.name = self 
+  end
+  
+  def add_post_by_title(post)
+    post_by_title = Post.new(post)
+    post_by_title = self
+  end 
+  
+  
