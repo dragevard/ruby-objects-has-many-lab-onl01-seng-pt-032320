a733c91103b13887f456c6c70708042c8774c99d
Derek phung <dragevards@gmail.com>
2020-04-29 20:01:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,11 @@
 class Artist 
-  attr_accessor :name
   
-  @@all =[]
+  attr_accessor :name 
+  
+  @@all = []
   
   def initialize(name)
     @name = name 
-    @all << self
-  end 
-  
-  def self.all
-    @@all 
-  end 
-  
-  def add_song(song)
-    song.artist = self
-  end 
-  
-  def add_song_by_name(song)
-    song_by_name = Song.new(song)
-    song.artist = self
-  end 
-  
-  def songs 
-    Song.all.select do |song|
-      song.artist == self 
-    end
-  end
-  
-  def self.song_count
-    Song.all.count 
+    @@all << self 
   end
 end
